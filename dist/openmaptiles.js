c207882e6eb01887e7c7d5b11812d29280dd7efd
ahocevar <andreas.hocevar@gmail.com>
2019-04-11 12:32:09 +0200

    Add dist for v4.3.0

---
+++ dist/openmaptiles.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{122:function(e,s,o){"use strict";o.r(s);o(61);var t=o(35),c=document.cookie.replace(/(?:(?:^|.*;\s*)tilehosting_access_token\s*\=\s*([^;]*).*$)|^.*$/,"$1");c||(c=window.prompt("Enter your tilehosting API access token:"),document.cookie="tilehosting_access_token="+c+"; expires=Fri, 31 Dec 9999 23:59:59 GMT"),Object(t.b)("map","https://maps.tilehosting.com/styles/basic/style.json?key="+c)}},[[122,0]]]);
+//# sourceMappingURL=openmaptiles.js.map
---
+++ dist/wms.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{126:function(n,s,w){"use strict";w.r(s);w(61);var a=w(35);Object(a.b)("map","data/wms.json")}},[[126,0]]]);
+//# sourceMappingURL=wms.js.map
