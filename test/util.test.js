ec4dc29911136ece493f88c12f8ce3b941413707
Andreas Hocevar <andreas.hocevar@gmail.com>
2020-03-22 11:05:51 +0100

    Preserve hard line breaks when wrapping text

--- test/util.test.js
+++ test/util.test.js
@@ -7,14 +7,20 @@ describe('util', function() {
 
     it('should not combine undefined when no next line exists', function() {
       const text = 'Shor T';
-      const result = wrapText(text, 'normal 600 12px/1.2 "Open Sans"', 10, 0);
+      const result = wrapText(text, 'normal 400 12px/1.2 sans-serif', 10, 0);
       should(result).equal(text);
     });
 
     it('should wrap text', function() {
       const text = 'Longer line of text for wrapping';
-      const result = wrapText(text, 'normal 600 12px/1.2 "Open Sans"', 10, 0);
+      const result = wrapText(text, 'normal 400 12px/1.2 sans-serif', 10, 0);
       result.includes('\n').should.be.true();
+    });
+
+    it('should preserve hard breaks', function() {
+      const text = 'Großer Sonnleitstein\n1639 m';
+      const result = wrapText(text, 'normal 400 12px/1.2 sans-serif', 10, 0);
+      should(result).equal('Großer\nSonnleitstein\n1639 m');
     });
   });
 });
