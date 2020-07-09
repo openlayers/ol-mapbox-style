c1de408848266eccb70edc4a3d0037d677b410d2
mike-000 <49240900+mike-000@users.noreply.github.com>
2020-07-09 13:42:41 +0100

    fix eslint

--- src/util.js
+++ src/util.js
@@ -14,24 +14,24 @@ import {labelCache} from 'ol/render/canvas';
 export const assign =
   typeof Object.assign === 'function'
     ? Object.assign
-    : function (target, var_sources) {
-        if (target === undefined || target === null) {
-          throw new TypeError('Cannot convert undefined or null to object');
-        }
+    : function(target, var_sources) {
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
 
-        const output = Object(target);
-        for (let i = 1, ii = arguments.length; i < ii; ++i) {
-          const source = arguments[i];
-          if (source !== undefined && source !== null) {
-            for (const key in source) {
-              if (source.hasOwnProperty(key)) {
-                output[key] = source[key];
-              }
+      const output = Object(target);
+      for (let i = 1, ii = arguments.length; i < ii; ++i) {
+        const source = arguments[i];
+        if (source !== undefined && source !== null) {
+          for (const key in source) {
+            if (source.hasOwnProperty(key)) {
+              output[key] = source[key];
             }
           }
         }
-        return output;
-      };
+      }
+      return output;
+    };
 
 export function deg2rad(degrees) {
   return degrees * Math.PI / 180;
