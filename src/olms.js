cc2f3122abaea98e2007a9279a427eec37622b54
Andreas Hocevar <andreas.hocevar@gmail.com>
2021-12-13 17:02:07 +0100

    Add stylefunction functions to legacy build

--- src/olms.js
+++ src/olms.js
@@ -1,7 +1,17 @@
 import olms, {apply, applyBackground, applyStyle} from './index.js';
-import stylefunction from './stylefunction.js';
+import stylefunction, {
+  recordStyleLayer,
+  renderTransparent,
+} from './stylefunction.js';
 import {assign} from './util.js';
 
-assign(olms, {apply, applyBackground, applyStyle, stylefunction});
+assign(olms, {
+  apply,
+  applyBackground,
+  applyStyle,
+  stylefunction,
+  recordStyleLayer,
+  renderTransparent,
+});
 
 export default olms;
