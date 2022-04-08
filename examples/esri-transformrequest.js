2bd625fea0ace4e8290c8af53ac75aa7c6234d44
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-04-08 20:06:44 +0200

    Fix typo

--- examples/esri-transformrequest.js
+++ examples/esri-transformrequest.js
@@ -6,7 +6,7 @@ olms(
   'https://www.arcgis.com/sharing/rest/content/items/2afe5b807fa74006be6363fd243ffb30/resources/styles/root.json',
   {
     transformRequest(url, type) {
-      if (type === 'Tile') {
+      if (type === 'Tiles') {
         url = url.replace(
           'World_Basemap_v2/tile/',
           'World_Basemap_v2/VectorTileServer/tile/'
