2c180e18ea4922f7d98d807338dc793b8836ba92
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-05-14 00:17:28 +0200

    Add dist for v8.0.8

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export { stylefunction, recordStyleLayer, renderTransparent } from "./stylefunction.js";
+export { apply as default, apply, applyBackground, applyStyle, getFeatureState, setFeatureState, getLayer, getLayers, getSource } from "./apply.js";
