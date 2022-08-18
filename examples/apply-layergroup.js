e91691d429ea26bbb3bce1ee0786d3c2c7e959e8
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-08-18 17:09:22 +0200

    Clean up and rename example

--- examples/apply-layergroup.js
+++ examples/apply-layergroup.js
@@ -1,9 +1,10 @@
 import 'ol/ol.css';
-import olms from 'ol-mapbox-style';
 import {Group as LayerGroup} from 'ol/layer.js';
 import {Map, View} from 'ol';
+import {apply} from 'ol-mapbox-style';
 
 const layerGroup = new LayerGroup();
+apply(layerGroup, 'data/geojson-wms.json');
 
 new Map({
   target: 'map',
@@ -13,9 +14,3 @@ new Map({
   }),
   layers: [layerGroup],
 });
-
-layerGroup.on('propertychange', () => {
-  console.info(layerGroup.getLayers().getArray()); //eslint-disable-line no-console
-});
-
-olms(layerGroup, 'data/geojson-wms.json');
