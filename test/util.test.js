openlayers/ol-mapbox-style

commit 4606f6cd4414d129e41d7ea60f7d33931ab644d7

Always use array for TileJSON's tiles property

src/util.js
@@ -117,10 +117,7 @@ export function getTileJson(glSource, styleUrl, options = {}) {
         promise = Promise.resolve(
           Object.assign({}, glSource, {
             url: undefined,
-            tiles:
-              glSource.type === 'raster'
-                ? [normalizedSourceUrl]
-                : normalizedSourceUrl,
+            tiles: [normalizedSourceUrl],
           })
         );
       } else {

commit aae892f20c6bbcda0bc36a2dd731daf796cea2d1

Fix Mapbox url regression introduced by 4606f6cd

src/util.js
@@ -1,3 +1,4 @@
+import {expandUrl} from 'ol/tileurlfunction.js';
 import {normalizeSourceUrl, normalizeStyleUrl} from './mapbox.js';
 
 export function deg2rad(degrees) {
@@ -115,7 +116,7 @@ export function getTileJson(glSource, styleUrl, options = {}) {
         promise = Promise.resolve(
           Object.assign({}, glSource, {
             url: undefined,
-            tiles: [normalizedSourceUrl],
+            tiles: expandUrl(normalizedSourceUrl),
           })
         );
       } else {

test/util.test.js
@@ -1,5 +1,5 @@
 import should from 'should';
-import {fetchResource} from '../src/util.js';
+import {fetchResource, getTileJson} from '../src/util.js';
 
 describe('util', function () {
   describe('fetchResource', function () {
@@ -14,6 +14,28 @@ describe('util', function () {
         should(typeof sprite.accommodation_camping).equal('object');
         done();
       });
+    });
+  });
+  describe('getTileJson', function () {
+    it('resolves mapbox:// tile urls properly', function (done) {
+      getTileJson(
+        {
+          url: 'mapbox://mapbox.mapbox-streets-v7',
+          type: 'vector',
+        },
+        '',
+        {accessToken: 'mytoken'}
+      )
+        .then(function (tilejson) {
+          should(tilejson.tiles).eql([
+            'https://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7/{z}/{x}/{y}.vector.pbf?access_token=mytoken',
+            'https://b.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7/{z}/{x}/{y}.vector.pbf?access_token=mytoken',
+            'https://c.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7/{z}/{x}/{y}.vector.pbf?access_token=mytoken',
+            'https://d.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7/{z}/{x}/{y}.vector.pbf?access_token=mytoken',
+          ]);
+          done();
+        })
+        .catch(done);
     });
   });
 });