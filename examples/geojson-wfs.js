46a9c10c2d2f68564cc3a659076f912ef6b40ca1
Andreas Hocevar <andreas.hocevar@gmail.com>
2022-12-23 19:59:09 +0100

    Remove unnecessary transformRequest

--- examples/geojson-wfs.js
+++ examples/geojson-wfs.js
@@ -1,12 +1,4 @@
 import 'ol/ol.css';
 import {apply} from 'ol-mapbox-style';
 
-apply('map', 'data/geojson-wfs.json', {
-  transformRequest: (urlStr, type) => {
-    if (type === 'GeoJSON') {
-      const url = new URL(urlStr + '&transformRequest=true');
-      const req = new Request(url);
-      return req;
-    }
-  },
-});
+apply('map', 'data/geojson-wfs.json');
