ec17137b76861a39ec3e90d82b1a121e0f5df7ce
Andreas Hocevar <andreas.hocevar@gmail.com>
2023-02-18 10:04:33 +0100

    Add dist for v9.6.0

---
+++ dist/text.d.ts
@@ -0,0 +1,8 @@
+export function applyLetterSpacing(text: any, letterSpacing: any): any;
+export function wrapText(text: any, font: any, em: any, letterSpacing: any): any;
+/**
+ * @param {Array} fonts Fonts.
+ * @return {Array} Processed fonts.
+ * @private
+ */
+export function getFonts(fonts: any[]): any[];
