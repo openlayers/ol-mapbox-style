1657da0695b3bc427c384548c80ad46cafe680b9
Andreas Hocevar <andreas.hocevar@gmail.com>
2023-06-01 13:09:59 +0200

    Add dist for v10.6.0

---
+++ dist/examples/openmaptiles.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkol_mapbox_style=self.webpackChunkol_mapbox_style||[]).push([[347],{4944:(e,s,o)=>{o(9789);var a=o(3216),c=document.cookie.replace(/(?:(?:^|.*;\s*)maptiler_access_token\s*\=\s*([^;]*).*$)|^.*$/,"$1");c||(c=window.prompt("Enter your MapTiler API access token:"),document.cookie="maptiler_access_token="+c+"; expires=Fri, 31 Dec 9999 23:59:59 GMT"),(0,a.ZP)("map","https://api.maptiler.com/maps/basic/style.json?key="+c)}},e=>{e(e.s=4944)}]);
+//# sourceMappingURL=openmaptiles.js.map
---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export { stylefunction, recordStyleLayer, renderTransparent, getStyleForLayer } from "./stylefunction.js";
+export { apply as default, apply, applyBackground, applyStyle } from "./apply.js";
+export { getFeatureState, setFeatureState, getLayer, getLayers, getSource, getMapboxLayer, updateMapboxLayer, addMapboxLayer, removeMapboxLayer } from "./util.js";
