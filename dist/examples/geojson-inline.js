c6f216870c5319e7ede36994db4863608172af33
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-05-22 22:13:50 +0200

    Add dist for v12.3.3

---
+++ dist/examples/geojson-inline.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkol_mapbox_style=self.webpackChunkol_mapbox_style||[]).push([[422],{8779:(s,e,a)=>{a(8508),(0,a(4316).Bb)("map","data/geojson-inline.json")}},s=>{s(s.s=8779)}]);
+//# sourceMappingURL=geojson-inline.js.map
---
+++ dist/examples/mapbox.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkol_mapbox_style=self.webpackChunkol_mapbox_style||[]).push([[626],{5103:(e,o,s)=>{s(8508);var c=s(4316);let a=document.cookie.replace(/(?:(?:^|.*;\s*)mapbox_access_token\s*\=\s*([^;]*).*$)|^.*$/,"$1");a||(a=window.prompt("Enter your Mapbox API access token:"),a&&(document.cookie="mapbox_access_token="+a+"; expires=Fri, 31 Dec 9999 23:59:59 GMT")),(0,c.Ay)("map","mapbox://styles/mapbox/bright-v9",{accessToken:a})}},e=>{e(e.s=5103)}]);
+//# sourceMappingURL=mapbox.js.map
