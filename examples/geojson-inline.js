2d996dd55e2af7f9e91f69ddb2c97a0a04e5c54f
ahocevar <ahocevar@users.noreply.github.com>
2025-01-28 12:03:29 +0000

    deploy: 1aa4e8da34368c0b79aa08c01f98e9a5abbe73ec

--- examples/geojson-inline.js
+++ examples/geojson-inline.js
@@ -1,2 +1,2 @@
-"use strict";(self.webpackChunkol_mapbox_style=self.webpackChunkol_mapbox_style||[]).push([[422],{8779:(s,e,a)=>{a(8508),(0,a(316).Bb)("map","data/geojson-inline.json")}},s=>{s(s.s=8779)}]);
+"use strict";(self.webpackChunkol_mapbox_style=self.webpackChunkol_mapbox_style||[]).push([[422],{8779:(s,e,a)=>{a(8508),(0,a(8384).Bb)("map","data/geojson-inline.json")}},s=>{s(s.s=8779)}]);
 //# sourceMappingURL=geojson-inline.js.map
