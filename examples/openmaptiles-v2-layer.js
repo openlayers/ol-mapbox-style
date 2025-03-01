d2f3b16d6fabd420147007181f51da8d9bf1aedd
Remo Geissbühler <37411349+remogeissbuehler@users.noreply.github.com>
2025-03-01 13:21:22 +0100

    add example with maptiler v2 tiles

---
+++ examples/openmaptiles-v2-layer.js
@@ -0,0 +1,31 @@
+import 'ol/ol.css';
+import {Map, View} from 'ol';
+import VectorTileLayer from 'ol/layer/VectorTile.js';
+import {applyBackground, applyStyle} from 'ol-mapbox-style';
+
+const baseUrl = 'https://api.maptiler.com/maps/basic-v2/style.json';
+
+let key = document.cookie.replace(
+  /(?:(?:^|.*;\s*)maptiler_access_token\s*\=\s*([^;]*).*$)|^.*$/,
+  '$1',
+);
+if (!key) {
+  key = window.prompt('Enter your MapTiler API access token:');
+  document.cookie =
+    'maptiler_access_token=' + key + '; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+}
+const styleUrl = baseUrl + '?key=' + key;
+
+const layer = new VectorTileLayer({
+  declutter: true,
+});
+applyStyle(layer, styleUrl);
+applyBackground(layer, styleUrl);
+new Map({
+  target: 'map',
+  layers: [layer],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
