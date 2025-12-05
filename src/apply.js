421edf8b2aee80f221be47affb810973914c0bfd
Jacob Miller <jmiller@anduril.com>
2025-12-04 16:05:59 -0800

    put rasters in same layer

--- src/apply.js
+++ src/apply.js
@@ -1176,12 +1176,7 @@ function processStyle(glStyle, mapOrGroup, styleUrl, options) {
     } else {
       id = glLayer.source || getSourceIdByRef(glLayers, glLayer.ref);
       // this technique assumes gl layers will be in a particular order
-      if (
-        // This line is because rasters set properties on their source
-        glLayer.type === 'raster' ||
-        !id ||
-        id != glSourceId
-      ) {
+      if (!id || id != glSourceId) {
         if (layerIds.length) {
           promises.push(
             finalizeLayer(
