5ac724ac342ccb59c60f4c603c02539c55dfc806
Shai Etzion <etzshai1@gmail.com>
2025-12-09 20:14:40 +0200

    feat: added support for GeometryCollections

--- src/stylefunction.js
+++ src/stylefunction.js
@@ -17,10 +17,12 @@ import {
   v8 as spec,
 } from '@maplibre/maplibre-gl-style-spec';
 import mb2css from 'mapbox-to-css-font';
+import {Feature} from 'ol';
 import Map from 'ol/Map.js';
 import {distance} from 'ol/coordinate.js';
 import {getCenter} from 'ol/extent.js';
 import {toPromise} from 'ol/functions.js';
+import {GeometryCollection} from 'ol/geom.js';
 import RenderFeature from 'ol/render/Feature.js';
 import Circle from 'ol/style/Circle.js';
 import Fill from 'ol/style/Fill.js';
@@ -554,7 +556,31 @@ export function stylefunction(
     cameraObj.zoom = zoom;
     cameraObj.distanceFromCenter = 0;
     const featureGeometry = feature.getGeometry();
+
+    if (
+      featureGeometry.getType &&
+      featureGeometry instanceof GeometryCollection
+    ) {
+      const geoms = featureGeometry.getGeometries();
+
+      for (const geom of geoms) {
+        const subFeature = new Feature({
+          ...feature.getProperties(),
+          geometry: geom,
+        });
+
+        const subStyles = styleFunction(subFeature, resolution, onlyLayer);
+
+        if (subStyles && Array.isArray(subStyles)) {
+          styles.push(...subStyles);
+        }
+      }
+
+      return styles.length > 0 ? styles : undefined;
+    }
+
     const type = types[featureGeometry.getType()];
+
     const map = olLayer.get('map');
     if (map && map instanceof Map && type === 1) {
       const size = map.getSize();
