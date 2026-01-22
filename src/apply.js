3536fdbf41ce7348ec7782bbf30553cc7739cac4
Andreas Hocevar <andreas.hocevar@gmail.com>
2026-01-22 20:35:29 +0100

    Ignore gl source for background layers

--- src/apply.js
+++ src/apply.js
@@ -963,11 +963,13 @@ export function setupLayer(glStyle, styleUrl, glLayer, options) {
   const glLayers = glStyle.layers;
   const type = glLayer.type;
 
-  const id = glLayer.source || getSourceIdByRef(glLayers, glLayer.ref);
-  const glSource = glStyle.sources[id];
+  let glSourceId = glLayer.source || getSourceIdByRef(glLayers, glLayer.ref);
+  const glSource = glStyle.sources[glSourceId];
   let layer;
   if (type == 'background') {
     layer = setupBackgroundLayer(glLayer, options, functionCache);
+    // background layers do not have a source
+    glSourceId = undefined;
   } else if (glSource.type == 'vector') {
     layer = setupVectorLayer(glSource, styleUrl, options);
   } else if (glSource.type == 'raster') {
@@ -1104,7 +1106,6 @@ export function setupLayer(glStyle, styleUrl, glLayer, options) {
       glLayer.layout ? glLayer.layout.visibility !== 'none' : true,
     );
   }
-  const glSourceId = id;
   if (layer) {
     layer.set('mapbox-source', glSourceId);
   }
@@ -1193,10 +1194,8 @@ function processStyle(glStyle, mapOrGroup, styleUrl, options) {
           !(layer instanceof VectorLayer || layer instanceof VectorTileLayer)
         ) {
           layerIds = [];
-          glSourceId = undefined;
-        } else {
-          glSourceId = layer.get('mapbox-source');
         }
+        glSourceId = layer.get('mapbox-source');
       }
       layerIds.push(glLayer.id);
     }
