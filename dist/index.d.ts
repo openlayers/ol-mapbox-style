7ca917e21a4d35d91a9c77c14bc8bec5dda0b850
Andreas Hocevar <andreas.hocevar@gmail.com>
2026-02-20 19:02:47 +0100

    Add dist for v13.3.0

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export { styleConfig } from "./expressions.js";
+export { default as MapboxVectorLayer } from "./MapboxVectorLayer.js";
+export { getStyleForLayer, recordStyleLayer, renderTransparent, stylefunction } from "./stylefunction.js";
+export { addMapboxLayer, apply, apply as default, applyBackground, applyStyle, getFeatureState, getLayer, getLayers, getMapboxLayer, getSource, removeMapboxLayer, setFeatureState, updateMapboxLayer, updateMapboxSource } from "./apply.js";
