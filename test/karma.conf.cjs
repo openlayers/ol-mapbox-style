97be004f9aad9be52b03db068677cfdf718bbddd
Andreas Hocevar <andreas.hocevar@gmail.com>
2026-06-20 13:35:54 +0200

    Use async puppeteer executablePath()

--- test/karma.conf.cjs
+++ test/karma.conf.cjs
@@ -1,14 +1,11 @@
 const {join} = require('path');
-const puppeteer = require('puppeteer');
-
-process.env.CHROME_BIN = puppeteer.executablePath();
 
 const flags = ['--headless=new'];
 if (process.env.CI) {
   flags.push('--no-sandbox');
 }
 
-module.exports = function (karma) {
+module.exports = async function (karma) {
   karma.set({
     browsers: ['ChromeHeadless'],
     customLaunchers: {
@@ -91,5 +88,5 @@ module.exports = function (karma) {
     },
   });
 
-  process.env.CHROME_BIN = require('puppeteer').executablePath();
+  process.env.CHROME_BIN = await require('puppeteer').executablePath();
 };
